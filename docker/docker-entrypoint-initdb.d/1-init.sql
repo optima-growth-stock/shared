9206b1cecaa0b07d3edd36fdb865a7dd0e26bcb2
Toni Vrdoljak <toni.vrdoljak4@gmail.com>
2022-07-02 12:18:03 +0200

    Add organization table to database

--- docker/docker-entrypoint-initdb.d/1-init.sql
+++ docker/docker-entrypoint-initdb.d/1-init.sql
@@ -1,8 +1,17 @@
-CREATE TABLE licenses (
-    license_id text PRIMARY KEY,
-    organization_id text NOT NULL,
-    description text,
-    product_name text NOT NULL,
-    license_type text NOT NULL,
-    comment text
+CREATE TABLE organizations (
+    organization_id TEXT PRIMARY KEY,
+    name TEXT NOT NULL,
+    contact_name TEXT NOT NULL,
+    contact_email TEXT NOT NULL,
+    contact_phone TEXT NOT NULL
 );
+
+CREATE TABLE licenses (
+    license_id TEXT PRIMARY KEY,
+    organization_id TEXT NOT NULL REFERENCES organizations(organization_id),
+    description TEXT,
+    product_name TEXT NOT NULL,
+    license_type TEXT NOT NULL,
+    comment TEXT
+);
+
